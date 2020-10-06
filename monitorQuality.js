b8277242cf7521afa4c0fe427cf300b874b94d43
Nick <nick-ls@users.noreply.github.com>
2020-10-05 23:15:56 -0700

    Fix reliability issues with quality setting
    I ended up converting this to a chrome extension that supplements the youtube player base.js with one that exposes the quality setting methods to the global scope, so that the extension can access them and set the quality of videos with much more success.

---
+++ monitorQuality.js
@@ -0,0 +1,61 @@
+const qualList = ["4320", "2160", "1440", "1080", "720", "large", "medium", "480", "360", "240", "144", "auto"];
+let maxQuality = qualList[2];
+function init() {
+	let mo = new MutationObserver(setQuality);
+	mo.observe(document.querySelector("div.ytp-spinner"), {attributes: true});
+	setQuality();
+}
+function setQuality() {
+	let availableQualities = Object.keys(quals.options);
+	let quality = getHighestQuality(availableQualities, maxQuality);
+	quals.G.setPlaybackQuality(quality);
+}
+function getHighestQuality(qualityList, max) {
+	let qualityIndex = qualList.indexOf(max);
+	while (qualityIndex < qualList.length) {
+		for (let quality in qualityList) {
+			if (qualityList[quality].includes(qualList[qualityIndex])) {
+				return qualityList[quality];
+			}
+		}
+		qualityIndex++;
+	}
+	return "auto";
+}
+_waitForSelector("button.ytp-button.ytp-settings-button").then(elem => {
+	elem.click();
+	elem.click();
+	console.log("waiting?")
+	_waitForVar("quals").then(init);
+});
+async function _waitForVar(windowVar) {
+	return new Promise((resolve, reject) => {
+		let interval = setInterval(function (v) {
+			if (window.hasOwnProperty(v)) {
+				clearInterval(interval);
+				resolve(window[v]);
+			}
+		}, 50, windowVar);
+	});
+}
+async function _waitForSelector(selector, content) {
+	console.log("waiting for", selector);
+	return new Promise((resolve, reject) => {
+		let interval = setInterval((s, c) => {
+			let elem = document.querySelectorAll(s);
+			[...elem].forEach(e => {
+				if (c) {
+					if (e.innerText === c) {
+						clearInterval(interval);
+						resolve(e)
+						return;
+					}
+				} else {
+					clearInterval(interval);
+					resolve(e);
+					return;
+				}
+			});
+		}, 50, selector, content);
+	});
+}
